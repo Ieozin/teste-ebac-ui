5d2dac5349b8d850fb0ebff9f49022b4b8489b27
leonardo <leonardomartinslm9090@gmail.com>
2024-11-13 21:17:30 -0300

    primeiro teste automazidado

---
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -0,0 +1,13 @@
+///<reference types="cypress"/>
+
+describe('Funcionalidade: Login', () => {
+
+    it('Deve fazer login com sucesso', () => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('leo.teste@teste.com')
+        cy.get('#password').type('leo123')
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, leo.teste (não é leo.teste? Sair)')
+    })
+})
