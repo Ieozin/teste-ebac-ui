8dfcf47c761e161e7e739e304e6eeeb222a1321e
leonardo <leonardomartinslm9090@gmail.com>
2024-11-19 20:12:50 -0300

    adicionando produtos ao teste

---
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -0,0 +1,24 @@
+///<reference types="cypress"/>
+
+describe('Funcionalidade: Produtos', () => {
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+   
+    it('Deve selecionar um produto da lista', () => {
+        cy.get('.product-block').first().click()
+        cy.get('#tab-title-description > a').should('contain', 'Descrição')
+    });
+
+    it('Deve selecionar o ultimo produto da lista', () => {
+        cy.get('.product-block').last().click()
+        cy.get('#tab-title-description > a').should('contain', 'Descrição') 
+    });
+
+    it('Deve selecionar o terceiro produto da lista', () => {
+        cy.get('.product-block').eq(2).click()
+        cy.get('#tab-title-description > a').should('contain', 'Descrição') 
+    });
+
+});
